cbc765739617249d5e9fb3bd516257063ca6fe7b
Seshachalam Malisetti <abbiya@gmail.com>
2018-08-10 15:14:24 +0530

    add comments, proper names for methods

--- app/src/main/java/com/abbiya/lists/ItemDao.java
+++ app/src/main/java/com/abbiya/lists/ItemDao.java
@@ -10,24 +10,35 @@ import java.util.List;
 
 @Dao
 public interface ItemDao {
+    // get all items
     @Query("SELECT * FROM item ORDER BY updated_at DESC, created_at")
     public List<Item> getAllItems();
 
+    // get an item by id
+    @Query("SELECT * FROM item WHERE uid = (:uid)")
+    public Item getItem(Integer uid);
+
+    // get all top level items
     @Query("SELECT * FROM item WHERE parent_id IS NULL ORDER BY updated_at DESC, created_at")
     public List<Item> getAllRoots();
 
+    // get children of an item
     @Query("SELECT * FROM item WHERE parent_id = (:uid) ORDER BY updated_at DESC, created_at")
     public List<Item> getAllItemsOfParent(Integer uid);
 
+    // search all items for content and return items
     @Query("SELECT * FROM item WHERE content LIKE (:content) ORDER BY updated_at DESC, created_at")
     public List<Item> findItemsWithContent(String content);
 
+    // search child items of an item for content
     @Query("SELECT * FROM item WHERE parent_id = (:uid) AND content LIKE (:content) ORDER BY updated_at DESC, created_at")
     public List<Item> searchItemsOfParent(Integer uid, String content);
 
+    // search only the top level root items for content
     @Query("SELECT * FROM item WHERE parent_id IS NULL AND content LIKE (:content) ORDER BY updated_at DESC, created_at")
     public List<Item> searchRoots(String content);
 
+    // get children count of a item
     @Query("SELECT COUNT(uid) FROM item WHERE parent_id = (:parentId)")
     public Integer countChildrenOfRoot(Integer parentId);
 
